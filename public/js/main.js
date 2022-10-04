088d0222ca31882579b71c9f27d013778dc8c365
Albert Valls <avalls89@gmail.com>
2022-10-04 19:57:07 +0200

    Done strictEquals function

---
+++ public/js/main.js
@@ -0,0 +1,10 @@
+import { strictEquals } from "./module/strictEquals.js";
+
+console.log(`   1	-  1   =>  ${strictEquals(1, 1)}`);
+console.log(`  NaN - NaN   =>  ${strictEquals(NaN, NaN)}`);
+console.log(`    0 - -0    =>  ${strictEquals(0, -0)}`);
+console.log(`   -0 - 0     =>  ${strictEquals(-0, 0)}`);
+console.log(`    1 - '1'   =>  ${strictEquals(1, "1")}`);
+console.log(` true - false =>  ${strictEquals(true, false)}`);
+console.log(`false - false =>  ${strictEquals(false, false)}`);
+console.log(`water - oil   =>  ${strictEquals("water", "oil")}`);
